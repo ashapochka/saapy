f5f85ebacf8760333c9b2dd7de0ca5108cdbc413
Andriy Shapochka <ashapochka@gmail.com>
2016-07-02 20:48:27 +0300

    improved making visible saapy in pytests

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,12 @@
-import os
+# import os
+# import sys
+#
+# sys.path.insert(0, os.path.abspath('..'))
+#
+# import saapy
+
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
-
-import saapy
+# add project root directory to python path to enable import of saapy
+if ".." not in sys.path:
+    sys.path.append('..')
